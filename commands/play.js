c02505214dd14c7749e4e8aff35838713b690ade
BillCurdMusic <45gzj9sm7r@privaterelay.appleid.com>
2025-12-11 01:03:53 -0600

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,1 +1,15 @@
+export const data = {
+  name: "play",
+  description: "Play a specific track by name."
+};
 
+export async function execute(interaction, playlist, playSpecific, joinChannel) {
+  const songName = interaction.options.getString("song");
+
+  if (!songName) {
+    return interaction.reply("You gotta tell me *what* to play, kid.");
+  }
+
+  joinChannel(interaction);
+  setTimeout(() => playSpecific(interaction, songName), 500);
+}
