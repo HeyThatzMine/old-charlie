f016f20020429ab9e57d44a0de16736a2f6a1455
HeyThatzMine <Bill@Billcurdmusic.net>
2025-12-14 10:27:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.MessageContent
-  ),
+  ],
   partials: [Partials.Channel]
 });
 
